4b0a2cf75fd7873cdb0a3f5a93e409f689431555
Michelle <mliuu3259@gmail.com>
2025-01-03 16:11:22 -0500

    fix build issue

--- src/components/Company.js
+++ src/components/Company.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React from 'react';
 
 function Company({ website, company }) {
   return (
-    <div className='w-full h-full flex justify-center py-4'>
-      <a className='px-6 my-auto' href={website} target='_blank'>
+    <div className="w-full h-full flex justify-center py-4">
+      <a
+        className="px-6 my-auto"
+        href={website}
+        target="_blank"
+        rel="noreferrer"
+      >
         <img
           alt={`${company} logo`}
           src={`media/${company}-logo.png`}
-          className='max-w-full mx-auto object-contain'
+          className="max-w-full mx-auto object-contain"
         />
       </a>
     </div>
